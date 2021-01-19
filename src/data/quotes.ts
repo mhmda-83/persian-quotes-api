94098e553de5a5e3ddfcf5e22680c67f5e79fbb0
Mohammad MohammadAlian <mralian.dev@gmail.com>
2021-01-19 19:23:10 +0330

    feat: add 5 new quote from Albert Einsten

---
+++ src/data/quotes.ts
@@ -0,0 +1,33 @@
+import { Quote } from '../quotes/model';
+
+const quotes: Quote[] = [
+  {
+    text: 'زمان یک توهم است.',
+    author: { name: 'آلبرت اینشتین' },
+    categories: [{ name: 'science' }],
+  },
+  {
+    text:
+      'منطق شما را از نقطه‌ی الف به نقطه‌ی ب می‌رساند. تخیل شما را به هرجایی می‌برد.',
+    author: { name: 'آلبرت اینشتین' },
+    categories: [{ name: 'life' }],
+  },
+  {
+    text: 'هر احمقی می‌تواند بداند. نکته در فهمیدن است.',
+    author: { name: 'آلبرت اینشتین' },
+    categories: [{ name: 'science' }],
+  },
+  {
+    text: 'کسی که اشتباه نکرده باشد تا به حال چیز جدیدی را امتحان نکرده است.',
+    author: { name: 'آلبرت اینشتین' },
+    categories: [{ name: 'life' }],
+  },
+  {
+    text:
+      'دو چیز بی نهایت است. جهان و احمقیت انسان. و من هنوز راجع به بی نهایت بودن جهان مطمئن نیستم.',
+    author: { name: 'آلبرت اینشتین' },
+    categories: [{ name: 'life' }],
+  },
+];
+
+export default quotes;
