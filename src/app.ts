15617d0d7bc276ee579f8a0d063ae33b822b5a61
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-21 02:03:01 +0330

    feat: add boilerplate express app  ✨

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Express } from 'express';
+
+import { createTelegrafBot } from './bot';
+import { getConfig } from './config';
+
+const config = getConfig();
+const app: Express = express();
+
+const bot = createTelegrafBot(config);
+app.use(bot.webhookCallback(config.webhookPath));
