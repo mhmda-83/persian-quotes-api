b4d039f7df55ebd52fbb9baac6d27da73e62a109
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-21 18:00:38 +0330

    fix: fix wrong naming while importing  🚚

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import express, { Express } from 'express';
 
 import { createTelegrafBot } from './bot';
 import { getConfig } from './config';
+import { Context } from './infra/bot/TelegrafContext';
 
 const config = getConfig();
 const app: Express = express();
 
-const bot = createTelegrafBot(config);
+const bot = createTelegrafBot(config.botToken, { contextType: Context });
 
 if (config.isProduction) app.use(bot.webhookCallback(config.webhookPath));
 else bot.launch();
