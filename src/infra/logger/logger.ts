fb8f5179f3ebdbbbce805f5042d562990dedbc76
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-21 17:46:27 +0330

    fix: turn logger interface to abstract class
    to be posible for typescript-ioc to use it

--- src/infra/logger/logger.ts
+++ src/infra/logger/logger.ts
@@ -1,6 +1,7 @@
-interface Logger {
+abstract class Logger {
   log: (...args: any[]) => void;
   info: (...args: any[]) => void;
+  warn: (...args: any[]) => void;
   error: (...args: any[]) => void;
 }
 
