891ed96a646f69aee5b7bc25e4fdb17b3eba599e
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-22 16:45:02 +0330

    feat: complete translate command ✨

--- src/botHandlers/commands/translate.ts
+++ src/botHandlers/commands/translate.ts
@@ -2,8 +2,15 @@ import { Middleware } from 'telegraf-ts';
 
 import { Actions, Context } from '../../infra/bot/context';
 
-const translateHandler: Middleware<Context> = (ctx) => {
+const translateHandler: Middleware<Context> = async (ctx) => {
   ctx.session.action = Actions.TEXT;
+  const randomQuote = await ctx.quoteService.getRandomQuote();
+  ctx.reply(
+    `${randomQuote.text}\n\n—${
+      randomQuote.author
+    }\n${randomQuote.categories.map((c) => `#${c}`).join(' ')}`,
+  );
+  ctx.reply('now send me the translated quote (just the text for now)');
 };
 
 export { translateHandler };
