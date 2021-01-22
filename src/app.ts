748958575d7664528bb92898c3f395e997983859
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-22 16:30:13 +0330

    feat: pass logger to bot creator ✨

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,20 @@ import express, { Express } from 'express';
 
 import { createTelegrafBot } from './bot';
 import { getConfig } from './config';
+import { ConsoleLogger } from './infra/logger';
 
 const config = getConfig();
+const logger = new ConsoleLogger();
+
 const app: Express = express();
 
-const bot = createTelegrafBot(config.botToken);
+const bot = createTelegrafBot(config.botToken, logger);
 if (config.isProduction) app.use(bot.webhookCallback(config.webhookPath));
-else bot.launch();
+else
+  bot
+    .launch()
+    .then(() => logger.log('bot launch successfully  ✅'))
+    .catch((err) => {
+      logger.log('following error occurred ❌:');
+      logger.error(err);
+    });
