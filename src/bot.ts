1c657e69b0385510544f0331354db9f8d3582d43
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-22 16:29:10 +0330

    feat: move bot to src 🚚

---
+++ src/bot.ts
@@ -0,0 +1,25 @@
+import { session, Telegraf } from 'telegraf-ts';
+import { TelegrafOptions } from 'telegraf-ts/typings/telegraf';
+
+import { handlersComposer } from './botHandlers';
+import { Logger } from './config';
+import { Actions, Context } from './infra/bot/context';
+
+function createTelegrafBot(
+  botToken: string,
+  logger: Logger,
+  options?: TelegrafOptions,
+) {
+  const bot: Telegraf<Context> = new Telegraf<Context>(botToken, options);
+  bot.context.logger = logger;
+  bot.use(session());
+  bot.use((ctx, next) => {
+    ctx.session.action = Actions.NONE;
+    next();
+  });
+  bot.start((ctx: Context) => ctx.reply(ctx.session.action));
+  bot.use(handlersComposer);
+  return bot;
+}
+
+export { createTelegrafBot };
