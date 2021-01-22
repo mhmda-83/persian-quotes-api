ce40ceae4ec4eb02f8bd532075ea6785a9438336
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-22 16:29:31 +0330

    feat: add session to bot context ✨

--- src/infra/bot/context.ts
+++ src/infra/bot/context.ts
@@ -1,9 +1,14 @@
 import { TelegrafContext } from 'telegraf-ts';
 
+import { Actions } from '../../data/botActions';
 import { Logger } from '../logger';
 
+interface CustomSession {
+  action: Actions;
+}
 interface Context extends TelegrafContext {
   logger: Logger;
+  session: CustomSession;
 }
 
-export { Context };
+export { Actions, Context };
