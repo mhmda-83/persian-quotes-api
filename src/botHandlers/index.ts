82e0bec39c40fee8e19b9d5a6a1fa50314507d5d
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-23 12:19:00 +0330

    feat: add event composer to handler composers ✨

--- src/botHandlers/index.ts
+++ src/botHandlers/index.ts
@@ -2,9 +2,11 @@ import { Composer } from 'telegraf-ts';
 
 import { Context } from '../infra/bot/context';
 import { commandComposer } from './commands';
+import { eventComposer } from './events';
 
 const handlersComposer = new Composer<Context>();
 
 handlersComposer.use(commandComposer);
+handlersComposer.use(eventComposer);
 
 export { handlersComposer };
