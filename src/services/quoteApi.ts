e4e5c63551d194714eea221630113cead52e1c61
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-23 12:14:39 +0330

    feat: return as QuoteViewModel in QuoteApi ✨

--- src/services/quoteApi.ts
+++ src/services/quoteApi.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-import { Quote } from '../model/quote';
+import { QuoteViewModel } from '../model/quote';
 
 interface QuotableQuote {
   author: string;
@@ -15,15 +15,20 @@ class QuoteApi {
     this.http = axios.create({ baseURL: 'https://api.quotable.io' });
   }
 
-  async getRandomQuote(): Promise<Quote> {
+  async getRandomQuote(): Promise<QuoteViewModel> {
     const response = await this.http.get<QuotableQuote>('/random');
 
     const { author, content, tags } = response.data;
 
-    const quote: Quote = {
+    const quote: QuoteViewModel = {
       author,
       text: content,
       categories: tags,
+      toMarkdown() {
+        return `${quote.text}\n\n—${quote.author}\n${quote.categories
+          .map((c) => `#${c}`)
+          .join(' ')}`;
+      },
     };
 
     return quote;
