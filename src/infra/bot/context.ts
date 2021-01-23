c70c2fe6d9eedb1a0bc31e63a42a38e03a88afc1
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-23 12:08:48 +0330

    feat: add translatedQuote to session ✨

--- src/infra/bot/context.ts
+++ src/infra/bot/context.ts
@@ -1,11 +1,13 @@
 import { TelegrafContext } from 'telegraf-ts';
 
 import { Actions } from '../../data/botActions';
+import { Quote } from '../../model/quote';
 import QuoteApi from '../../services/quoteApi';
 import { Logger } from '../logger';
 
 interface CustomSession {
   action: Actions;
+  translatedQuote: Partial<Quote>;
 }
 interface Context extends TelegrafContext {
   logger: Logger;
