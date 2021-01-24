fe4d31531b8e05adc66627f179ddefbb26e78ee9
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-24 20:16:24 +0330

    fix: fix mongoose repo wrong implimitation 🐛

--- src/repository/mongooseQuote.ts
+++ src/repository/mongooseQuote.ts
@@ -5,24 +5,23 @@ import { TranslatedQuote } from '../model/translatedQuote';
 import QuoteRepo, { QueryOptions } from './quote';
 
 class MongooseQuoteRepo implements QuoteRepo {
-  public async getAll(options: QueryOptions) {
+  public async getAll(options: QueryOptions): Promise<TranslatedQuote[]> {
     const quotes = await MongooseQuoteModel.find().setOptions(options);
-
     return quotes;
   }
 
-  public async getCount() {
+  public async getCount(): Promise<number> {
     const count = await MongooseQuoteModel.countDocuments();
 
     return count;
   }
 
-  public async getById(id: string) {
+  public async getById(id: string): Promise<TranslatedQuote | null> {
     const quote = await MongooseQuoteModel.findById(id);
     return quote;
   }
 
-  public async getRandom() {
+  public async getRandom(): Promise<TranslatedQuote | null> {
     const quotes: MongooseQuoteDoc[] = await MongooseQuoteModel.aggregate([
       { $sample: { size: 1 } },
     ]);
@@ -32,7 +31,7 @@ class MongooseQuoteRepo implements QuoteRepo {
     return quote;
   }
 
-  public async getCategories() {
+  public async getCategories(): Promise<string[]> {
     const quotes = await MongooseQuoteModel.find();
 
     let categories: string[] = [];
@@ -43,13 +42,16 @@ class MongooseQuoteRepo implements QuoteRepo {
     return categories;
   }
 
-  public async getCategoriesCount() {
+  public async getCategoriesCount(): Promise<number> {
     const categories = await this.getCategories();
 
-    return categories.length;
+    return categories?.length ?? 0;
   }
 
-  public async getByCategory(category: string, options: QueryOptions) {
+  public async getByCategory(
+    category: string,
+    options: QueryOptions,
+  ): Promise<TranslatedQuote[] | null> {
     const quotes = await MongooseQuoteModel.find({
       'translated.categories': category,
     }).setOptions(options);
@@ -57,7 +59,9 @@ class MongooseQuoteRepo implements QuoteRepo {
     return quotes;
   }
 
-  public async getRandomByCategory(category: string) {
+  public async getRandomByCategory(
+    category: string,
+  ): Promise<TranslatedQuote | null> {
     const quotes: MongooseQuoteDoc[] = await MongooseQuoteModel.aggregate([
       { $match: { 'translated.categories': category } },
       { $sample: { size: 1 } },
@@ -68,20 +72,23 @@ class MongooseQuoteRepo implements QuoteRepo {
     return quote;
   }
 
-  public async getAuthors() {
+  public async getAuthors(): Promise<string[]> {
     const quotes = await MongooseQuoteModel.find();
     const authors: string[] = quotes.map((quote) => quote.translated.author);
 
     return authors;
   }
 
-  public async getAuthorsCount() {
+  public async getAuthorsCount(): Promise<number> {
     const authors = await this.getAuthors();
 
-    return authors.length;
+    return authors?.length ?? 0;
   }
 
-  public async getByAuthor(author: string, options: QueryOptions) {
+  public async getByAuthor(
+    author: string,
+    options: QueryOptions,
+  ): Promise<TranslatedQuote[] | null> {
     const quotes = await MongooseQuoteModel.find({
       'translated.author': author,
     }).setOptions(options);
@@ -89,7 +96,9 @@ class MongooseQuoteRepo implements QuoteRepo {
     return quotes;
   }
 
-  public async getRandomByAuthor(author: string) {
+  public async getRandomByAuthor(
+    author: string,
+  ): Promise<TranslatedQuote | null> {
     const quotes: MongooseQuoteDoc[] = await MongooseQuoteModel.aggregate([
       { $match: { 'translated.author': author } },
       { $sample: { size: 1 } },
@@ -100,10 +109,34 @@ class MongooseQuoteRepo implements QuoteRepo {
     return quote;
   }
 
-  public async insertOne(quote: TranslatedQuote) {
+  public async insertOne(
+    quote: TranslatedQuote,
+  ): Promise<TranslatedQuote | null> {
     const insertedQuote = await MongooseQuoteModel.create(quote);
 
     return insertedQuote;
+  }
+
+  public async updateVerificationById(
+    quoteId: string,
+    verificationState: boolean,
+  ): Promise<MongooseQuoteDoc | null> {
+    const updatedQuote = await MongooseQuoteModel.findByIdAndUpdate(
+      quoteId,
+      {
+        verified: verificationState,
+      },
+      { returnOriginal: false },
+    );
+    return updatedQuote;
+  }
+
+  public async removeById(quoteId: string): Promise<boolean> {
+    const removedDoc = await MongooseQuoteModel.findByIdAndRemove(quoteId);
+    if (removedDoc) {
+      return true;
+    }
+    return false;
   }
 }
 
