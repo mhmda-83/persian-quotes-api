09befc3488f78fa51a8190212b961c212cc2c1f5
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-24 20:11:27 +0330

    fix: fix id conflict between TranslatedQuote and Document 🐛

--- src/model/mongooseQuoteModel.ts
+++ src/model/mongooseQuoteModel.ts
@@ -3,7 +3,9 @@ import mongoose, { Document } from 'mongoose';
 import schema from './mongooseQuoteSchema';
 import { TranslatedQuote } from './translatedQuote';
 
-export interface MongooseQuoteDoc extends TranslatedQuote, Document {}
+export interface MongooseQuoteDoc extends TranslatedQuote, Document {
+  id: string;
+}
 
 const model = mongoose.model<MongooseQuoteDoc>('quote', schema);
 
