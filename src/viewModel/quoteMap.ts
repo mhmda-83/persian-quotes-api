be4a2ab6cc997251f09c4e89c2f4a193fcc7a27c
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-24 19:50:06 +0330

    feat: add translationView to QuoteMap ✨

--- src/viewModel/quoteMap.ts
+++ src/viewModel/quoteMap.ts
@@ -9,6 +9,20 @@ const QuoteMap = {
     const mappedCats = categories.map((c) => `#${c}`).join(' ');
     return `${text}\n\n—${author}\n${mappedCats}`;
   },
+
+  translationView({
+    original,
+    translated,
+  }: {
+    original: Partial<Quote>;
+    translated: Partial<Quote>;
+  }) {
+    return (
+      `${QuoteMap.toView(original)}\n` +
+      `ترجمه شد به\n\n` +
+      `${QuoteMap.toView(translated)}`
+    );
+  },
 };
 
 export { QuoteMap };
