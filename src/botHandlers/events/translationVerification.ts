eff8f37e932d36c7844ce1d402c43237ee9c62b3
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-25 16:37:00 +0330

    feat: add delete countdown ✨

--- src/botHandlers/events/translationVerification.ts
+++ src/botHandlers/events/translationVerification.ts
@@ -9,12 +9,20 @@ const translationVerification: Middleware<Context> = async (ctx) => {
     const [state, docId] = data.split('-');
     if (state === TranslationState.VERIFIED) {
       await ctx.repo.updateVerificationById(docId, true);
-      ctx.reply('saved successfully 🎉');
+      ctx.editMessageText(
+        'saved successfully 🎉\n\n this message will be deleted in 5 seconds',
+      );
     } else if (state === TranslationState.DECLINED) {
       const isRemoved = await ctx.repo.removeById(docId);
-      if (isRemoved) ctx.reply('removed from the database successfully 🎉');
+      if (isRemoved)
+        ctx.editMessageText(
+          'removed from the database successfully 🎉\n\nthis message will be deleted in 5 seconds',
+        );
       else ctx.reply('it seems something went wrong while trying to delete 🤔');
     }
+    setTimeout(() => {
+      ctx.deleteMessage();
+    }, 5000);
   }
 };
 
