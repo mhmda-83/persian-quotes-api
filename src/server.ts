95107ac51f9f45624b1690696a779c13927177ae
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-28 23:54:44 +0330

    fix: call init for initialing app 🐛

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
 import { Container } from 'typescript-ioc';
 
 import { ExpressApp } from './app';
-import { TelegrafBot } from './bot';
-import { getConfig } from './config';
 import { createContainer } from './config/container';
+import { sampleRecords } from './data/seedRecord';
 
 Container.configure(...createContainer());
 
 const app = new ExpressApp();
-const bot = new TelegrafBot();
-const config = getConfig();
 
-if (config.isProduction) app.useMiddleware(bot.lunchUsingWebhook());
-else bot.launchUsingPooling();
+app.init({ seedingData: sampleRecords });
+app.listen();
