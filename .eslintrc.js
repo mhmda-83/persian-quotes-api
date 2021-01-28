617b776340451d5127b94276bfdee42899369598
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-28 18:20:21 +0330

    fix: upper case enum members 🐛

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: ['@fullstacksjs'],
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'enumMember',
+        format: ['UPPER_CASE'],
+      },
+    ],
+  },
 };
