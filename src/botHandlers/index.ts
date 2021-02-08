e48308ada89950655e2207ed8c29374e9297ca72
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-02-08 14:30:15 +0330

    fix: move start handler to its own file 🐛

--- src/botHandlers/index.ts
+++ src/botHandlers/index.ts
@@ -1,25 +1,11 @@
 import { Composer } from 'telegraf-ts';
 
-import { DefaultState } from '../data/botStates';
 import { Context } from '../infra/bot/context';
 import { commandComposer } from './commands';
 import { eventComposer } from './events';
 
 const handlersComposer = new Composer<Context>();
 
-handlersComposer.start((ctx) => {
-  if (ctx.session) ctx.session.state = DefaultState.NONE;
-  ctx.reply(
-    'سلام 👋🏻\n\n' +
-      'به ربات ثبت ترجمه ی نقل قول خوش آمدی 🤖\n' +
-      'برای ترجمه /translate رو ارسال کن تا برات یک نقل و قول بصورت رندوم بفرستم\n\n' +
-      'در ضمن api و این ربات open source هستند\n' +
-      'اگه دوست داشتی میتونی یه نگاه بهش بندازی ' +
-      '<a href="https://github.com/mhmda-83/persian-quotes-api">لینک</a>',
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    { parse_mode: 'HTML', disable_web_page_preview: true },
-  );
-});
 handlersComposer.use(commandComposer);
 handlersComposer.use(eventComposer);
 
