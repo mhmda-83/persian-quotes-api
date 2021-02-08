df8f8729f3825ce4e67470ab4b216ef5b1c25f71
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-02-08 16:10:56 +0330

    fix: add start to event composer 🐛

--- src/botHandlers/events/index.ts
+++ src/botHandlers/events/index.ts
@@ -1,11 +1,13 @@
 import { Composer } from 'telegraf-ts';
 
 import { Context } from '../../infra/bot/context';
+import { startHandler } from './start';
 import { translateHandler } from './translate';
 import { translationVerification } from './translationVerification';
 
 const eventComposer = new Composer<Context>();
 
+eventComposer.start(startHandler);
 eventComposer.on('text', translateHandler);
 eventComposer.on('callback_query', translationVerification);
 
