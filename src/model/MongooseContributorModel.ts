20c571074274a047a188b74376a8afbadd5a0d93
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-02-10 23:24:24 +0330

    fix: remove id prop in MongooseContributorDoc 🐛

--- src/model/MongooseContributorModel.ts
+++ src/model/MongooseContributorModel.ts
@@ -3,9 +3,7 @@ import mongoose, { Document } from 'mongoose';
 import { Contributor } from './contributor';
 import { mongooseContributorSchema } from './MongooseContributorSchema';
 
-export interface MongooseContributorDoc extends Contributor, Document {
-  id: string;
-}
+export interface MongooseContributorDoc extends Contributor, Document {}
 
 const MongooseContributorModel = mongoose.model<MongooseContributorDoc>(
   'contributor',
