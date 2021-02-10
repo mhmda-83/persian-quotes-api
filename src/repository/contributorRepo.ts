e6f7ec591c7f8a8c484215c67332a09d11bd209c
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-02-10 23:35:49 +0330

    feat: add connect and insertOne ✨

--- src/repository/contributorRepo.ts
+++ src/repository/contributorRepo.ts
@@ -2,7 +2,9 @@ import { Contributor } from '../model/contributor';
 import { QueryOptions } from './queryOptions';
 
 abstract class ContributorRepo {
+  connect: () => void;
   getByTelegramId: (telegramId: string) => Promise<Contributor | null>;
+  insertOne: (contributor: Contributor) => Promise<Contributor>;
   getAll: (options: QueryOptions) => Promise<Contributor[]>;
 }
 
--- src/repository/mongooseContributor.ts
+++ src/repository/mongooseContributor.ts
@@ -1,3 +1,7 @@
+import mongoose from 'mongoose';
+import { Inject, InjectValue } from 'typescript-ioc';
+
+import { Config, Logger } from '../config';
 import { ContributorMapper } from '../mapper/contributorMapper';
 import { Contributor } from '../model/contributor';
 import {
@@ -8,6 +12,28 @@ import { ContributorRepo } from './contributorRepo';
 import { QueryOptions } from './queryOptions';
 
 class MongooseContributorRepo implements ContributorRepo {
+  @Inject private logger: Logger;
+  @InjectValue('config') private config: Config;
+
+  public connect() {
+    mongoose
+      .connect(this.config.databaseUrl, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        this.logger.info('connected to database successfully ⚙');
+      })
+      .catch((err) => {
+        this.logger.info(
+          'following error occurred while connecting to database ❌:',
+        );
+        this.logger.error(err);
+      });
+  }
+
   async getByTelegramId(telegramId: string): Promise<Contributor | null> {
     const contributor: MongooseContributorDoc = await MongooseContributorModel.findOne(
       {
@@ -16,6 +42,14 @@ class MongooseContributorRepo implements ContributorRepo {
     ).lean();
     return ContributorMapper.toDomain(contributor);
   }
+
+  async insertOne(contributor: Contributor): Promise<Contributor> {
+    const insertedContributor = await MongooseContributorModel.create(
+      contributor,
+    );
+    return ContributorMapper.toDomain(insertedContributor);
+  }
+
   getAll: (options: QueryOptions) => Promise<Contributor[]>;
 }
 
