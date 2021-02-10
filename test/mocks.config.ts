965c16a4495d8e88329c21dd2bc16f420594abbd
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-02-11 01:35:49 +0330

    test: add ContributorRepo mock 🤡

--- test/mocks.config.ts
+++ test/mocks.config.ts
@@ -2,6 +2,8 @@ import { ConstantConfiguration, ContainerConfiguration } from 'typescript-ioc';
 
 import { Logger } from '../src/config';
 import { ConsoleLogger } from '../src/infra/logger';
+import { ContributorRepo } from '../src/repository/contributorRepo';
+import { MongooseContributorRepo } from '../src/repository/mongooseContributorRepo';
 import MongooseQuoteRepo from '../src/repository/mongooseQuote';
 import QuoteRepo from '../src/repository/quote';
 
@@ -16,5 +18,6 @@ export function createContainer(): ContainerConfig {
     { bindName: 'config', to: createConfig() },
     { bind: Logger, to: ConsoleLogger },
     { bind: QuoteRepo, to: MongooseQuoteRepo },
+    { bind: ContributorRepo, to: MongooseContributorRepo },
   ];
 }
