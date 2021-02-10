671e7b0f70c12e566b8a5978ed903ea84106a81a
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-02-11 01:36:18 +0330

    test: add test for get by telegramId ✅

---
+++ test/repository/MongooseContributorRepo.test.ts
@@ -0,0 +1,32 @@
+import chai, { expect } from 'chai';
+import chaiThings from 'chai-things';
+import { Container } from 'typescript-ioc';
+
+import { ContributorRepo } from '../../src/repository/contributorRepo';
+import { contributorSamples } from '../data/contributorSamples';
+import { createContainer } from '../mocks.config';
+
+chai.should();
+chai.use(chaiThings);
+
+describe('mongooseContributorRepo', () => {
+  Container.configure(...createContainer());
+  const repo = Container.get(ContributorRepo);
+
+  before('connect to db', () => {
+    repo.connect();
+  });
+
+  beforeEach(async () => {
+    await repo.deleteAll();
+    await repo.seed(contributorSamples);
+  });
+
+  it('should return document by telegramId', async () => {
+    const [sample] = contributorSamples;
+    const doc = await repo.getByTelegramId(sample.telegramId);
+    expect(doc?.contributionCount)
+      .to.be.a('number')
+      .and.eq(sample.contributionCount);
+  });
+});
