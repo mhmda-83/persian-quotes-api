f330794e9fdf0a062de7dfb23e122d18e4abd159
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-02-11 00:04:35 +0330

    feat: add contributor samples ✨

---
+++ test/data/contributorSamples.ts
@@ -0,0 +1,11 @@
+import { Contributor } from '../../src/model/contributor';
+
+const contributorSamples: Contributor[] = [
+  { contributionCount: 1, telegramId: '23153124' },
+  { contributionCount: 4, telegramId: '23523235' },
+  { contributionCount: 15, telegramId: '13114234' },
+  { contributionCount: 12, telegramId: '55555545' },
+  { contributionCount: 7, telegramId: '44513253' },
+];
+
+export { contributorSamples };
