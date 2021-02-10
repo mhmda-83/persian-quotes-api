80d9f23e551d39acf247b2ac32ca9d15afe8104f
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-02-10 23:27:14 +0330

    fix: fix ContributorRepo not returning a promise 🐛

--- src/repository/contributorRepo.ts
+++ src/repository/contributorRepo.ts
@@ -2,7 +2,7 @@ import { Contributor } from '../model/contributor';
 import { QueryOptions } from './queryOptions';
 
 abstract class ContributorRepo {
-  getByTelegramId: (telegramId: string) => Promise<Contributor>;
+  getByTelegramId: (telegramId: string) => Promise<Contributor | null>;
   getAll: (options: QueryOptions) => Promise<Contributor[]>;
 }
 
