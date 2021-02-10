0751ff8b3b4da206988a8bdf91e98236c58b511a
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-02-10 23:28:43 +0330

    feat: implement getByTelegramId ✨

--- src/repository/mongooseContributor.ts
+++ src/repository/mongooseContributor.ts
@@ -1,9 +1,21 @@
+import { ContributorMapper } from '../mapper/contributorMapper';
 import { Contributor } from '../model/contributor';
+import {
+  MongooseContributorDoc,
+  MongooseContributorModel,
+} from '../model/MongooseContributorModel';
 import { ContributorRepo } from './contributorRepo';
 import { QueryOptions } from './queryOptions';
 
 class MongooseContributorRepo implements ContributorRepo {
-  getByTelegramId: (telegramId: string) => Promise<Contributor>;
+  async getByTelegramId(telegramId: string): Promise<Contributor | null> {
+    const contributor: MongooseContributorDoc = await MongooseContributorModel.findOne(
+      {
+        telegramId,
+      },
+    ).lean();
+    return ContributorMapper.toDomain(contributor);
+  }
   getAll: (options: QueryOptions) => Promise<Contributor[]>;
 }
 
